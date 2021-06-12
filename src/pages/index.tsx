493a3d0916930e8c190a7b0dc2f69fc1aa4aa548
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-06-12 20:51:49 -0300

    :recycle: Customize the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, Welcome</span>
+          <h1>News about <br/> the  <span>React</span> world.</h1>
+          <p>Get access to all the publications <br /> 
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
